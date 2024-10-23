eb1274a48c9b6f00bb81f245ea0828cf7808019f
devseren4 <159440493+devseren4@users.noreply.github.com>
2024-10-23 14:33:59 +0300

    flash_cards: show back card after delay

--- 2410_flash_card/main.py
+++ 2410_flash_card/main.py
@@ -9,12 +9,23 @@ BACKGROUND_COLOR = "#B1DDC6"
 
 data = pandas.read_csv("data/french_words.csv")
 to_learn = data.to_dict(orient="records")
+current_card = ""
 
 
 def next_card():
+    global current_card
     current_card = random.choice(to_learn)
-    canvas.itemconfig(title_text, text="French")
-    canvas.itemconfig(word_text, text=current_card["French"])
+    canvas.itemconfig(canvas_img, image=card_front_img)
+    canvas.itemconfig(title_text, text="French", fill="black")
+    canvas.itemconfig(word_text, text=current_card["French"], fill="black")
+    window.after(3000, after)
+
+
+def after():
+    global current_card
+    canvas.itemconfig(canvas_img, image=card_back_img)
+    canvas.itemconfig(title_text, text="English", fill="white")
+    canvas.itemconfig(word_text, text=current_card["English"], fill="white")
 
 
 # ---------------------------------------- UI ----------------------------------- #
@@ -31,7 +42,9 @@ window.config(
 # CARD_FRONT
 canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
 card_front_img = PhotoImage(file="images/card_front.png")
-canvas.create_image(400, 263, image=card_front_img)
+card_back_img = PhotoImage(file="images/card_back.png")
+
+canvas_img = canvas.create_image(400, 263, image=card_front_img)
 title_text = canvas.create_text(
     400, 150, text="", fill="black", font=(FONT_NAME, 40, "italic")
 )
@@ -51,5 +64,6 @@ right_button = Button(image=right_button_img, highlightthickness=0, command=next
 right_button.grid(row=1, column=1)
 
 next_card()
+
 
 window.mainloop()
