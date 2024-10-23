51263a2df95719c851708e7e7e99d659325ea052
devseren4 <159440493+devseren4@users.noreply.github.com>
2024-10-23 12:11:07 +0300

    flash_card: add buttons to UI

--- 2410_flash_card/main.py
+++ 2410_flash_card/main.py
@@ -23,7 +23,15 @@ title_text = canvas.create_text(
 word_text = canvas.create_text(
     400, 263, text="Word", fill="black", font=(FONT_NAME, 60, "bold")
 )
-canvas.grid(row=0, column=0)
+canvas.grid(row=0, column=0, columnspan=2)
 
+# WRONG BUTTON
+wrong_button_img = PhotoImage(file="images/wrong.png")
+wrong_button = Button(image=wrong_button_img, highlightthickness=0)
+wrong_button.grid(row=1, column=0)
 
+# RIGHT BUTTON
+right_button_img = PhotoImage(file="images/right.png")
+right_button = Button(image=right_button_img, highlightthickness=0)
+right_button.grid(row=1, column=1)
 window.mainloop()
