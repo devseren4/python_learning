5429ac353bd7eb9280b31e760e742919d263bffe
devseren4 <159440493+devseren4@users.noreply.github.com>
2024-10-23 16:08:02 +0300

    flash_card: save words to learn to file

--- 2410_flash_card/main.py
+++ 2410_flash_card/main.py
@@ -3,22 +3,28 @@ import pandas
 import random
 
 FONT_NAME = "Ariel"
-
 BACKGROUND_COLOR = "#B1DDC6"
+current_card = {}
+to_learn = {}
 
 
-data = pandas.read_csv("data/french_words.csv")
-to_learn = data.to_dict(orient="records")
-current_card = ""
+try:
+    data = pandas.read_csv("data/words_to_learn.csv")
+except FileNotFoundError:
+    original_data = pandas.read_csv("data/french_words.csv")
+    to_learn = original_data.to_dict(orient="records")
+else:
+    to_learn = data.to_dict(orient="records")
 
 
 def next_card():
-    global current_card
+    global current_card, flip_timer
+    window.after_cancel(flip_timer)
     current_card = random.choice(to_learn)
     canvas.itemconfig(canvas_img, image=card_front_img)
     canvas.itemconfig(title_text, text="French", fill="black")
     canvas.itemconfig(word_text, text=current_card["French"], fill="black")
-    window.after(3000, after)
+    flip_timer = window.after(3000, func=after)
 
 
 def after():
@@ -26,6 +32,13 @@ def after():
     canvas.itemconfig(canvas_img, image=card_back_img)
     canvas.itemconfig(title_text, text="English", fill="white")
     canvas.itemconfig(word_text, text=current_card["English"], fill="white")
+
+
+def is_known():
+    to_learn.remove(current_card)
+    data = pandas.DataFrame(to_learn)
+    data.to_csv("data/words_to_learn.csv", index=False)
+    next_card()
 
 
 # ---------------------------------------- UI ----------------------------------- #
@@ -38,7 +51,7 @@ window.config(
     padx=50,
     pady=50,
 )
-
+flip_timer = window.after(3000, func=after)
 # CARD_FRONT
 canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
 card_front_img = PhotoImage(file="images/card_front.png")
@@ -60,7 +73,7 @@ wrong_button.grid(row=1, column=0)
 
 # RIGHT BUTTON
 right_button_img = PhotoImage(file="images/right.png")
-right_button = Button(image=right_button_img, highlightthickness=0, command=next_card)
+right_button = Button(image=right_button_img, highlightthickness=0, command=is_known)
 right_button.grid(row=1, column=1)
 
 next_card()
