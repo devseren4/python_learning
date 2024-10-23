6d9a6c4bfae2faa15cd8dc71e743281c508fe992
devseren4 <159440493+devseren4@users.noreply.github.com>
2024-10-23 13:47:01 +0300

    flash_cards: next word on button click

--- 2410_flash_card/main.py
+++ 2410_flash_card/main.py
@@ -1,8 +1,23 @@
 from tkinter import *
+import pandas
+import random
 
 FONT_NAME = "Ariel"
 
 BACKGROUND_COLOR = "#B1DDC6"
+
+
+data = pandas.read_csv("data/french_words.csv")
+to_learn = data.to_dict(orient="records")
+
+
+def next_card():
+    current_card = random.choice(to_learn)
+    canvas.itemconfig(title_text, text="French")
+    canvas.itemconfig(word_text, text=current_card["French"])
+
+
+# ---------------------------------------- UI ----------------------------------- #
 
 # WINDOW
 window = Tk()
@@ -16,22 +31,25 @@ window.config(
 # CARD_FRONT
 canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
 card_front_img = PhotoImage(file="images/card_front.png")
-canvas.create_image(400, 260, image=card_front_img)
+canvas.create_image(400, 263, image=card_front_img)
 title_text = canvas.create_text(
-    400, 150, text="Title", fill="black", font=(FONT_NAME, 40, "italic")
+    400, 150, text="", fill="black", font=(FONT_NAME, 40, "italic")
 )
 word_text = canvas.create_text(
-    400, 263, text="Word", fill="black", font=(FONT_NAME, 60, "bold")
+    400, 263, text="", fill="black", font=(FONT_NAME, 60, "bold")
 )
 canvas.grid(row=0, column=0, columnspan=2)
 
 # WRONG BUTTON
 wrong_button_img = PhotoImage(file="images/wrong.png")
-wrong_button = Button(image=wrong_button_img, highlightthickness=0)
+wrong_button = Button(image=wrong_button_img, highlightthickness=0, command=next_card)
 wrong_button.grid(row=1, column=0)
 
 # RIGHT BUTTON
 right_button_img = PhotoImage(file="images/right.png")
-right_button = Button(image=right_button_img, highlightthickness=0)
+right_button = Button(image=right_button_img, highlightthickness=0, command=next_card)
 right_button.grid(row=1, column=1)
+
+next_card()
+
 window.mainloop()
