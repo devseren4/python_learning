e185812efde2e036dc254ac97069662829e8f666
devseren4 <159440493+devseren4@users.noreply.github.com>
2024-10-29 13:13:30 +0200

    first_game_with_pygame: show player

--- 2410_first_game_with_pygame/main.py
+++ 2410_first_game_with_pygame/main.py
@@ -12,13 +12,27 @@ pygame.display.set_caption("Galaxion")
 icon = pygame.image.load("alien.png")
 pygame.display.set_icon(icon)
 
+# Player
+playerImg = pygame.image.load("player.png")
+# Values for the player placement
+playerX = 370
+playerY = 480
+
+
+def player():
+    screen.blit(playerImg, (playerX, playerY))
+
+
 # Game loop
 running = True
 while running:
+
+    # changing the screen color
+    screen.fill((31, 62, 90))
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    # changing the screen color
-    screen.fill((31, 62, 90))
+    player()
     pygame.display.update()
