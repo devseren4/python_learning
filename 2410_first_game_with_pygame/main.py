642206af88a02c53924f6c2e7b08c7f63e7f252d
devseren4 <159440493+devseren4@users.noreply.github.com>
2024-10-29 12:44:58 +0200

    first_game_with_pygame: change the Title and the Background color and add an Icon

--- 2410_first_game_with_pygame/main.py
+++ 2410_first_game_with_pygame/main.py
@@ -7,8 +7,18 @@ pygame.init()
 # create the screen
 screen = pygame.display.set_mode((800, 600))
 
+# Ttile and Icon
+pygame.display.set_caption("Galaxion")
+icon = pygame.image.load("alien.png")
+pygame.display.set_icon(icon)
+
+# Game loop
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+    # changing the screen color
+    screen.fill((31, 62, 90))
+    pygame.display.update()
