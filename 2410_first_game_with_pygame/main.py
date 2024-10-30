4342a81f12c2db3b25fdbb7440d7977da27026a2
devseren4 <159440493+devseren4@users.noreply.github.com>
2024-10-30 12:46:39 +0200

    first_game_with_pygame: changing the background

--- 2410_first_game_with_pygame/main.py
+++ 2410_first_game_with_pygame/main.py
@@ -7,6 +7,9 @@ pygame.init()
 # create the screen
 screen = pygame.display.set_mode((800, 600))
 
+# background
+background = pygame.image.load("background.png")
+
 # Ttile and Icon
 pygame.display.set_caption("Galaxion")
 icon = pygame.image.load("alien.png")
@@ -25,7 +28,7 @@ enemyImg = pygame.image.load("enemy.png")
 # making the enemy spawn random
 enemyX = random.randint(0, 800)
 enemyY = random.randint(50, 150)
-enemyX_change = 0.2
+enemyX_change = 3
 enemyY_change = 30
 
 
@@ -44,6 +47,8 @@ while running:
 
     # changing the screen color
     screen.fill((31, 62, 90))
+    # background image
+    screen.blit(background, (0, 0))
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -52,9 +57,9 @@ while running:
         # if key is pressed check if its right or left
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                playerX_change = -0.2
+                playerX_change = -5
             if event.key == pygame.K_RIGHT:
-                playerX_change = 0.2
+                playerX_change = 5
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                 playerX_change = 0
@@ -68,10 +73,10 @@ while running:
     # enemy moving and  adding coliision with wall for enemy
     enemyX += enemyX_change
     if enemyX <= 0:
-        enemyX_change = 0.2
+        enemyX_change = 3
         enemyY += enemyY_change
     elif enemyX >= 736:
-        enemyX_change = -0.2
+        enemyX_change = -3
         enemyY += enemyY_change
 
     player(playerX, playerY)
