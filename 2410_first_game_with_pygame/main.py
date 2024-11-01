6eb07bc95707396ee966d743559b1a40db7b698c
devseren4 <159440493+devseren4@users.noreply.github.com>
2024-11-01 13:49:09 +0200

    first_game_with_pygame: show the score on screeen

--- 2410_first_game_with_pygame/main.py
+++ 2410_first_game_with_pygame/main.py
@@ -52,7 +52,18 @@ bulletY_change = 10
 # ready - you can't see the bullet on the screen
 # fire - the bullet is currently moving
 bullet_state = "ready"
-score = 0
+
+
+# Score
+score_value = 0
+font = pygame.font.Font("freesansbold.ttf", 32)
+textX = 10
+textY = 10
+
+
+def show_score(x, y):
+    score = font.render("Score : " + str(score_value), True, (255, 255, 255))
+    screen.blit(score, (x, y))
 
 
 def player(x, y):
@@ -132,8 +143,7 @@ while running:
         if collision:
             bulletY = 480
             bullet_state = "ready"
-            score += 1
-            print(score)
+            score_value += 1
             enemyX[i] = random.randint(0, 735)
             enemyY[i] = random.randint(50, 150)
 
@@ -149,5 +159,5 @@ while running:
         bulletY -= bulletY_change
 
     player(playerX, playerY)
-
+    show_score(textX, textY)
     pygame.display.update()
