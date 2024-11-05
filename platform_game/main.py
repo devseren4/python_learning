b166f17f8a6c02c84af5579aef0bb6cea9091fff
devseren4 <159440493+devseren4@users.noreply.github.com>
2024-11-05 12:37:52 +0200

    platform_game: creating the surface

---
+++ platform_game/main.py
@@ -0,0 +1,23 @@
+import pygame
+from sys import exit
+
+
+pygame.init()
+screen = pygame.display.set_mode((800, 400))
+pygame.display.set_caption("Runny")
+clock = pygame.time.Clock()
+
+sky_surface = pygame.image.load("graphics/sky.png")
+ground_surface = pygame.image.load("graphics/ground.png")
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+
+    screen.blit(sky_surface, (0, 0))
+    screen.blit(ground_surface, (0, 300))
+
+    pygame.display.update()
+    clock.tick(60)
